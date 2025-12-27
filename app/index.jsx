394a695aea5ec2227c1370cec6803c6cbb67ccf8
joy <baohtrang@gmail.com>
2025-12-27 11:23:03 -0800

    Added number input section

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import StartGameScreen from "../screens/StartGameScreen";
+
+export default function index() {
+  return (
+    <StartGameScreen />
+  );
+}
+
+const style = StyleSheet.create({})
--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,10 +1,41 @@
-import { TextInput, View } from "react-native"
+import { StyleSheet, TextInput, View } from "react-native"
+import PrimaryButton from "../components/PrimaryButton"
 
 export default function StartGameScreen() {
     return (
-        <View>
-            <TextInput />
-            {/* Buttons */}
+        <View style={style.inputContainer}>
+            <TextInput style={style.numberInput} maxLength={2} />
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
         </View>
     )
 };
+
+const style = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        backgroundColor: '#72063c',
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowRadius: 6,
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.3,
+        alignItems: 'center',
+    },
+
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    }
+
+})
